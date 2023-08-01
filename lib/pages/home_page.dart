19fbf4065f832b383e71565733c7280c3ce6cb29
Amar <amarjaiswar1606@gmail.com>
2023-08-01 17:36:28 +0530

    Day 14 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -48,11 +48,52 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-            itemCount: CatalogModel.items.length,
-            itemBuilder: (context, index) {
-              return ItemWidget(item: CatalogModel.items[index]);
-            }),
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 16,
+            crossAxisSpacing: 16,
+          ),
+          itemBuilder: (context, index) {
+            final item = CatalogModel.items[index];
+            return Card(
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child: GridTile(
+                header: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(
+                    color: Colors.deepPurple,
+                  ),
+                  child: Text(
+                    item.name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                footer: Container(
+                  padding: const EdgeInsets.all(12),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                  ),
+                  child: Text(
+                    item.price.toString(),
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                ),
+                child: Image.network(item.image),
+              ),
+            );
+          },
+          itemCount: CatalogModel.items.length,
+        ),
+        // ListView.builder(
+        //     itemCount: CatalogModel.items.length,
+        //     itemBuilder: (context, index) {
+        //       return ItemWidget(item: CatalogModel.items[index]);
+        //     }),
       ),
       drawer: const MyDrawer(),
     );
