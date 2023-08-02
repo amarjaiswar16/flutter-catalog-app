cbb47558ff333aea91ad729ccb1163f4cd4c1289
Amar <amarjaiswar1606@gmail.com>
2023-08-02 13:58:43 +0530

    Day 25 completed

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/cart.dart';
 
 import 'package:velocity_x/velocity_x.dart';
@@ -17,7 +18,7 @@ class CartPage extends StatelessWidget {
       body: Column(children: [
         _CartList().p32().expand(),
         const Divider(),
-       const  _CartTotal(),
+        const _CartTotal(),
       ]),
     );
   }
@@ -28,7 +29,8 @@ class _CartTotal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final _cart = CartModel();
+    final CartModel _cart = (VxState.store as MyStore).cart;
+
     return SizedBox(
       height: 200,
       child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
@@ -59,7 +61,7 @@ class _CartTotal extends StatelessWidget {
 class _CartList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final _cart = CartModel();
+     final CartModel _cart = (VxState.store as MyStore).cart;
 
     return _cart.items.isEmpty
         ? 'Nothing to show'.text.xl3.makeCentered()
